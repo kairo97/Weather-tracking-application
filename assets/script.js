7f1e6153afcb48e5875ddb2023d3d5b795c45374
Kairo97 <109006341+kairo97@users.noreply.github.com>
2023-01-11 20:22:53 -0800

    have all apis links, keep getting 403 error saying api key is forbidden

--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,101 @@
 var searchBar = document.querySelector('#searchbar');
 var city = document.querySelector('#city-results');
 var searchBtn = document.querySelector('#search')
-var genralApi = "https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=2377ce6170fd23501dbc0eba2329d594";
-var geoApi1 = "http://api.openweathermap.org/geo/1.0/direct?q="
-var geoApi2 = "&limit=5&appid=2377ce6170fd23501dbc0eba2329d594";
-var weatherApi = "https://api.openweather.org/data/3.0/onecall?lat="
-var resultsArea = document.querySelector('#resultsArea');
+var resultsArea = document.querySelector('#resultsArea')
+var mapQapiKey = "?key=8choJFlxyk4M6cqDGGvwd930sIqc7mjL&"
+var mapQapi = "https://www.mapquestapi.com/geocoding/v1/address"
+searchBtn.addEventListener('click', getWeather);{
+     input = mapQapi + mapQapiKey + '&location=' + searchBar.value;
+    ;
+}
 
-function searchCity(city){
- 
-fetch("https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=10&appid=2377ce6170fd23501dbc0eba2329d594").then(function(res){
+function getWeather() {
+
+var weatherApi = "https://api.openweather.org/data/2.5/weather?"
+var forecastApi = "https://api.openweathermap.org/data/2.5/forecast?"
+var weatherApiKey = "appid=2377ce6170fd23501dbc0eba2329d594"
+var weather = {
+    city: '',
+    state: '',
+    date: '',
+    weatherType: '',
+    temp: '',
+    humidity: '',
+    windSpeed: '',
+};
+var forecast = [];
+fetch(mapQapi).then(function(res){
     return res.json();
 }).then(function(data){
-    renderResults(data)
-})
-}
-function renderResults(resultData){
-    console.log(resultData)
-    var newDiv = document.createElement('div')
-    var newCard = document.createElement('card')
-    newCard.textContent = resultData;
-    newDiv.append(newCard);
-    resultsArea.append(newDiv);
-    
+    if(data.results[0].locations[0].adminArea3 !=""){
+        weather.state =", " +data.results[0].locations[0].adminArea3 + ", ";
+    } else{
+        weather.state =", "
+    }
+    returnedLat = 'lat=' + data.results[0].locations[0].latLng.lat;
+    returnedLng = '&lon=' + data.results[0].locations[0].latLng.lng;
+
+    weatherApi += returnedLat +returnedLng + weatherApiKey;
+    forecastApi += returnedLat +returnedLng + weatherApiKey;
+
+    fetch(weatherApi).then(function(res){
+        return res.json();
+    }).then(function(data){
+        weather.city = data.name;
+        weather.state += data.sys.country;
+        weather.weatherType = data.weather[0],main;
+        weather.temp = Math.round(1.8*(data.main.temp - 273) + 32);
+        weather.humidity = data.main.humidity;
+        weather.windSpeed = Math.round(data.wind.speed);
+        input.value('')
+    });
+    fetch(forecastApi).then(function(res){
+        return res.json();
+    }).then(function(data){
+        var dayCount = 0;
+        var currentDay;
+        var day1 = {
+            date: '',
+            weather: [],
+            temp:  [],
+            humidity: [],
+            windSpeed: [],
+        };
+        var day2 = {
+            date: '',
+            weather: [],
+            temp:  [],
+            humidity: [],
+            windSpeed: [],
+        };
+        var day3 = {
+            date: '',
+            weather: [],
+            temp:  [],
+            humidity: [],
+            windSpeed: [],
+        };
+        var day4 = {
+            date: '',
+            weather: [],
+            temp:  [],
+            humidity: [],
+            windSpeed: [],
+        };
+        var day5 = {
+            date: '',
+            weather: [],
+            temp:  [],
+            humidity: [],
+            windSpeed: [],
+        };
+        forecast = [day1, day2, day3, day4, day5];
+        for (var i = 0; i < data.list.length; i++) {
+            currentDay = dayjs.unix(data.list[i].dt).format(MM/DD/YYYY)
+            if(currentDay === jayjs.format(MM/DD/YYYY)){}
+        }
+        
+        }
+)})
 
 }
-
-searchBtn.addEventListener('click',function(e){
-    e.preventDefault();
-    var searchTerm= searchBar.value;
-    searchCity(searchTerm);
-    city.textContent = `${searchTerm}`
-})
-// function getLatLon(lat, lon){
-// fetch("https://api.openweather.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=hourly,daily&appid=2377ce6170fd23501dbc0eba2329d594").then(function(resl){
-//     return resl.json();
-// }).then(function(temp)
-// }
-
-// array index 3 and 4 with lat and lon
